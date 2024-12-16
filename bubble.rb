0f707d0ed80ca757b566dec37863a63121b4f4bb
ALECHAPA912 <chapale2000@gmail.com>
2024-12-16 18:43:34 -0300

    Add files via upload

---
+++ bubble.rb
@@ -0,0 +1,18 @@
+arr = [4,3,78,2,0,2]
+
+def bubble_sort(array)
+  return array if array.size <= 1
+  swap = true
+    while swap
+      swap = false
+      (array.length - 1).times do |x|
+        if array[x] > array[x+1]
+          array[x], array[x+1] = array[x+1], array[x]
+          swap = true
+        end
+      end
+    end
+  array
+end
+
+p bubble_sort(arr)
